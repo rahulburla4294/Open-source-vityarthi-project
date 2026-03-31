b005d6a394694bdd2eef6765a3cd575922ecdd7a
rahulburla4294 <burlarahul4@gmail.com>
2026-03-31 19:45:59 +0530

    Update author name in manifesto generator script

--- script5_manifesto_generator.sh
+++ script5_manifesto_generator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script 5: Open Source Manifesto Generator
-# Author: Ajinkya Wasnik
+# Author: Burla Rahul
 # Course: Open Source Software
 
 echo "Answer the following questions to generate your Open Source Manifesto."
